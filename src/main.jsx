1551eec884f0ef577ad420bf5dc2b5194675950f
zillly <gil-hell@hotmail.com>
2023-06-19 22:29:17 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Details from './components/Details';
+import NotFound from './components/Home/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
@@ -13,7 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Navigate to={"/home"} />} />
       <Route path="/home" element={<Home />} />
       <Route path="/details/:id" element={<Details />} />
-      
-      </Routes>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+
   </BrowserRouter>
 );
